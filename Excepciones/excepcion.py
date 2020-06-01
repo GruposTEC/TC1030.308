4d0581d3ac3a497ba4bc96fb5ce9766f70df81bc
Antonio Mejia <avmejia@tec.mx>
2020-06-01 07:54:45 -0500

    Codigo 1 junio

--- Excepciones/excepcion.py
+++ Excepciones/excepcion.py
@@ -8,3 +8,28 @@ else:
 
 mal = direc + "prueba_1.txt"
 ok = direc + "prueba.txt"
+
+# try:
+#f = open(mal)
+#var = novar
+# except FileNotFoundError as e:
+# print(e)
+# except NameError:
+#print("error de nombre")
+# except Exception:
+#print("cachador universal")
+
+#x = 10
+#
+# if(x > 5):
+#raise Exception(f'la variable x no puede ser mayor a 5, el valor fue {x}')
+
+
+try:
+    f = open(mal)
+except FileNotFoundError:
+    print("no existe el archivo")
+else:
+    print("No hubo excepcion")
+finally:
+    print("yo siempre corro")
